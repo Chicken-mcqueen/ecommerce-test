3fa8fe57744bfc4d47a0585208af2afa931154a2
geraldvalente01@gmail.com <geraldvalente01@gmail.com>
2025-10-04 03:22:02 +0800

    7th commit

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
-const products = [
-  { name: "Product 1", price: 1200, image: "https://via.placeholder.com/220x150?text=Product+1" },
-  { name: "Product 2", price: 1500, image: "https://via.placeholder.com/220x150?text=Product+2" },
-  { name: "Product 3", price: 800,  image: "https://via.placeholder.com/220x150?text=Product+3" },
-  { name: "Product 4", price: 2000, image: "https://via.placeholder.com/220x150?text=Product+4" }
+const categorySamples = [
+  { name: "Electronics & Gadgets", image: "images/laptops.jpg" },
+  { name: "Fashion & Apparel", image: "images/dress.jpg" }
+
 ];
 
 const productsContainer = document.getElementById("products-container");
-products.forEach(product => {
+
+categorySamples.forEach(category => {
   const cardLink = document.createElement("a");
   cardLink.href = "products.html"; 
-  cardLink.classList.add("product-link"); 
+  cardLink.classList.add("product-link");
 
   const card = document.createElement("div");
   card.classList.add("product-card");
   card.innerHTML = `
-    <img src="${product.image}" alt="${product.name}">
-    <h3>${product.name}</h3>
-    <p>₱${product.price.toLocaleString()}</p>
-    <button>View</button>
+    <img src="${category.image}" alt="${category.name}">
+    <h3>${category.name}</h3>
   `;
 
   cardLink.appendChild(card);
